36a5bc7a86c52e1c45fa3e94ec635f4479f538f4
Marcelo Zoccoler <marcelozoc@gmail.com>
2022-11-09 14:28:42 +0100

    Update phasor plots
    - add median filters
    - add phasor plot to napari

--- notebooks/FLIM_phasor.py
+++ notebooks/FLIM_phasor.py
@@ -56,26 +56,13 @@ def get_phasor_components(flim_data, harmonic=1):
     flim_data_fft = np.fft.fft(flim_data, axis=0)
     dc = flim_data_fft[0].real
     # change the zeros to the img average
-    dc = np.where(dc != 0, dc, int(np.mean(dc)))
+    # dc = np.where(dc != 0, dc, int(np.mean(dc)))
+    dc = np.where(dc != 0, dc, 1)
     g = flim_data_fft[harmonic].real
     g = g / dc
     s = abs(flim_data_fft[harmonic].imag)
     s /= dc
     return g, s, dc
-
-# def get_phasor_components2(flim_data, harmonic=1):
-#     '''
-#     Calculate phasor components G and S from the fourier transform
-#     '''
-#     import numpy as np
-#     dc = np.sum(flim_data, axis=0)
-#     # change the zeros to the img average
-#     dc = np.where(dc != 0, dc, int(np.mean(dc)))
-#     g = np.sum(flim_data[:,0,0] * np.cos(2*np.pi*frequency*1E6*time_array*1E-9), axis=0) )
-#     g = g / dc
-#     s = np.sum(flim_data[:,0,0] * np.sin(2*np.pi*frequency*1E6*time_array*1E-9), axis=0) )
-#     s /= dc
-#     return g, s, dc
 
 
 def add_phasor_circle(ax):
